e4c28a7e1495e79bd262be649a64430d53e95368
Julien GIL <jgil@e1r4p11.42.fr>
2015-10-26 14:22:10 +0100

    update player

--- game/Player.cpp
+++ game/Player.cpp
@@ -32,10 +32,24 @@ Player &	Player::operator=( Player const & rhs )
 std::pair<int, int>		Player::play(Board* currentBoard, std::pair<int, int> xy)
 {
 	std::cout << this->_name << " playing" << std::endl;
-	std::pair<int, int> key = std::make_pair(4, 5);
-	(void)currentBoard;
-	(void)xy;
-	return key;
+	// std::pair<int, int> key = std::make_pair(4, 5);
+	// (void)currentBoard;
+	// (void)xy;
+	// return key;
+	this->referee(currentBoard, xy);
+	return xy;
+}
+
+bool					Player::referee(Board* currentBoard, std::pair<int, int> key)
+{
+	int		x = key.first;
+	int		y = key.second;
+
+	if (x > 18 || x < 0 || y > 18 || y < 0)
+		return ( false );
+	if (currentBoard->findPawn(x, y))
+		return (false);
+	return ( true );
 }
 
 int						Player::getName() const
